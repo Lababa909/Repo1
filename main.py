a6f00aef4e6c2be4a235c5845237d180d980366c
Coding Lababa <24919296-CodingLababa@users.noreply.replit.com>
2024-08-27 13:12:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+
+def numbers():
+  number1 = int(input("Enter starting number: "))
+  number2 = int(input("Enter ending number: "))
+  numbers()
+  if number2 < number1:
+    print("Error: ending number must be greater than starting number")
+    numbers()
+number1 = int(input("Enter starting number: "))
+number2 = int(input("Enter ending number: "))
+answer = 0
+bro = number1
+while bro <= number2:
+  answer += bro
+  bro += 1
+amount = number2-number1
+result = answer/amount
+print(result)
